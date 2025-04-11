a1ae4994f3b69d7d143877dba40f7cbd763d26e6
nova_44056 <senguy44056@gmail.com>
2025-04-11 17:48:30 -0500

    Merge branch 'refactorHomeComp' into 'hackerpackfix'

--- pages/hackerpacks/index.tsx
+++ pages/hackerpacks/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type { NextPage } from 'next';
-import HackerpackDisplay from '../../components/hackerpackComponents/HackerpackDisplay';
-import hackerpackSettings from '../../components/hackerpackComponents/hackerpack-settings.json';
+import HackerpackDisplay from '@/components/hackerpack/HackerpackDisplay';
+import hackerpackSettings from '@/components/hackerpack/hackerpack-settings.json';
 
 const HackerpacksIndexPage: NextPage = () => {
   // Use type assertion to tell TypeScript this is the correct type
@@ -10,14 +10,11 @@ const HackerpacksIndexPage: NextPage = () => {
     collection: {},
     collection_view: {},
     notion_user: {},
-    space: {}
+    space: {},
   } as any; // Use 'as any' to bypass type checking
 
   return (
-    <HackerpackDisplay 
-      content={emptyContent} 
-      notionRootId={hackerpackSettings.notionPageId} 
-    />
+    <HackerpackDisplay content={emptyContent} notionRootId={hackerpackSettings.notionPageId} />
   );
 };
 
